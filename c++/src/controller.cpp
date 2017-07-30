2537f20060f5fea7a1169168e8ba0d0448aa72da
kuya <kuya@gwu.edu>
2017-07-30 17:39:56 -0400

    Static function to class with parameters

--- c++/src/controller.cpp
+++ c++/src/controller.cpp
@@ -2,46 +2,46 @@
 using namespace Eigen;
 using namespace std;
 
-void controller::GeometricPositionController(node& node, Vector3d xd, Vector3d xd_dot, Vector3d xd_2dot,Vector3d Wd, Vector3d Wd_dot, Vector3d x, Vector3d v, Vector3d W_in, Matrix3d R){
+void controller::GeometricPositionController(Vector3d xd, Vector3d xd_dot, Vector3d xd_2dot,Vector3d Wd, Vector3d Wd_dot, Vector3d x, Vector3d v, Vector3d W_in, Matrix3d R){
 //   std::cout.precision(5);
   Vector3d xd_3dot, xd_4dot, b1d, b1d_dot, b1d_2dot;
   xd_3dot = xd_4dot = b1d_dot = b1d_2dot = Vector3d::Zero();
-  b1d = node.b1d_ned;
+  b1d = _params.b1d_ned;
   Vector3d e3(0,0,1);
   // convention conversion
   Vector3d W = W_in;
-  node.R = R; node.W = W; node.v = v; node.x = x;
-  b1d_dot = node.R_conv*b1d_dot;
-  b1d_2dot = node.R_conv*b1d_2dot;
+  _params.R = R; _params.W = W; _params.v = v; _params.x = x;
+  b1d_dot = _params.R_conv*b1d_dot;
+  b1d_2dot = _params.R_conv*b1d_2dot;
 
   // Translational Error Functions
   Vector3d ex = x - xd;
   Vector3d ev = v - xd_dot;
-  node.eX = ex; node.eV = ev;
+  _params.eX = ex; _params.eV = ev;
 
   // Saturation function
-  // node.eiX = node.eiX_last+node.del_t*(ex+node.cX*ev);
-  // err_sat(-node.eiX_sat, node.eiX_sat, node.eiX);
-  node.eiX_last = node.eiX;
+  // _params.eiX = _params.eiX_last+_params.del_t*(ex+_params.cX*ev);
+  // err_sat(-_params.eiX_sat, _params.eiX_sat, _params.eiX);
+  _params.eiX_last = _params.eiX;
 
   // Force 'f' along negative b3-axis
-  float kx = node.kx;
-  //float kxr = node.kxr;
-  float kv = node.kv;
-  float kiX = node.kiX;
-  float kR = node.kR;
-  float kRr = node.kRr;
-  float kW = node.kW;
-  float kiR = node.kiR;
-  float m = node.m;
-  float g = node.g;
+  float kx = _params.kx;
+  //float kxr = _params.kxr;
+  float kv = _params.kv;
+  float kiX = _params.kiX;
+  float kR = _params.kR;
+  float kRr = _params.kRr;
+  float kW = _params.kW;
+  float kiR = _params.kiR;
+  float m = _params.m;
+  float g = _params.g;
 
   //Matrix3d kxm = Vector3d(kx,kx,kx*kxr).asDiagonal();
-  Vector3d A = -kx*ex-kv*ev-kiX*node.eiX-m*g*e3+m*xd_2dot;
+  Vector3d A = -kx*ex-kv*ev-kiX*_params.eiX-m*g*e3+m*xd_2dot;
   Vector3d L = R*e3;
   Vector3d Ldot = R*hat_eigen(W)*e3;
   float f = -A.dot(R*e3);
-  node.f_total = f;
+  _params.f_total = f;
 
   // Intermediate Terms for Rotational Errors
   Vector3d ea = g*e3-f/m*L-xd_2dot;
@@ -78,40 +78,40 @@ void controller::GeometricPositionController(node& node, Vector3d xd, Vector3d x
   Rd << Rd1, Rd2, Ld;
   Rd_dot << Rd1dot, Rd2dot, Ld_dot;
   Rd_2dot << Rd1_2dot, Rd2_2dot, Ld_2dot;
-  node.Rc = Rd;
-  node.Rc_dot = Rd_dot;
-  node.Rc_2dot = Rd_2dot;
+  _params.Rc = Rd;
+  _params.Rc_dot = Rd_dot;
+  _params.Rc_2dot = Rd_2dot;
   // Vector3d Wd, Wd_dot;
   vee_eigen(Rd.transpose()*Rd_dot, Wd);
   vee_eigen(Rd.transpose()*Rd_2dot-hat_eigen(Wd)*hat_eigen(Wd), Wd_dot);
-  // TODO: node.Wc and node.Wc_dot
-  node.Wc = Wd; node.Wc_dot = Wd_dot;
+  // TODO: _params.Wc and _params.Wc_dot
+  _params.Wc = Wd; _params.Wc_dot = Wd_dot;
   // Attitude Error 'eR'
-  vee_eigen(.5*(Rd.transpose()*R-R.transpose()*Rd), node.eR);
+  vee_eigen(.5*(Rd.transpose()*R-R.transpose()*Rd), _params.eR);
   // Angular Velocity Error 'eW'
-  node.eW = W-R.transpose()*Rd*Wd;
+  _params.eW = W-R.transpose()*Rd*Wd;
   // Attitude Integral Term
-  node.eiR = node.del_t*(node.eR+node.cR*node.eW) + node.eiR_last;
-  err_sat(-node.eiR_sat, node.eiR_sat, node.eiR);
-  node.eiR_last = node.eiR;
+  _params.eiR = _params.del_t*(_params.eR+_params.cR*_params.eW) + _params.eiR_last;
+  err_sat(-_params.eiR_sat, _params.eiR_sat, _params.eiR);
+  _params.eiR_last = _params.eiR;
   // 3D Moment
   Matrix3d kRm = Vector3d(kR,kR,kR*kRr).asDiagonal();
-  node.M = -kRm*node.eR-kW*node.eW-kiR*node.eiR
-    +hat_eigen(R.transpose()*Rd*Wd)*node.J*R.transpose()*Rd*Wd
-    +node.J*R.transpose()*Rd*Wd_dot;
+  _params.M = -kRm*_params.eR-kW*_params.eW-kiR*_params.eiR
+    +hat_eigen(R.transpose()*Rd*Wd)*_params.J*R.transpose()*Rd*Wd
+    +_params.J*R.transpose()*Rd*Wd_dot;
 
   Matrix<double, 4, 1> FM;
   FM[0] = f;
-  FM[1] = node.M[0];
-  FM[2] = node.M[1];
-  FM[3] = node.M[2];
+  FM[1] = _params.M[0];
+  FM[2] = _params.M[1];
+  FM[3] = _params.M[2];
 
-  // node.f_motor = node.Ainv*FM;
+  // _params.f_motor = _params.Ainv*FM;
 }
 //
-// void controller::GeometricControl_SphericalJoint_3DOF(node& node, Vector3d Wd, Vector3d Wddot, Vector3d Win, Matrix3d Rin){
+// void controller::GeometricControl_SphericalJoint_3DOF(_params& _params, Vector3d Wd, Vector3d Wddot, Vector3d Win, Matrix3d Rin){
 //
-//   Matrix3d D = node.R_conv;
+//   Matrix3d D = _params.R_conv;
 //   Matrix3d R = D*Rin;// LI<-LBFF
 //   Vector3d W = Win;// LBFF
 //
@@ -120,31 +120,31 @@ void controller::GeometricPositionController(node& node, Vector3d xd, Vector3d x
 //   Vector3d e3(0,0,1), b3(0,0,1), vee_3by1;
 //   double l = 0;//.05;// length of rod connecting to the spherical joint
 //   Vector3d r = -l * b3;
-//   Vector3d F_g = node.m * node.g * R.transpose() * e3;
+//   Vector3d F_g = _params.m * _params.g * R.transpose() * e3;
 //   Vector3d M_g = r.cross(F_g);
 //   //   Calculate eR (rotation matrix error)
 //   Matrix3d inside_vee_3by3 = Rd.transpose() * R - R.transpose() * Rd;
 //   eigen_invskew(inside_vee_3by3, vee_3by1);// 3x1
-//   node.eR = vee_3by1/2;
+//   _params.eR = vee_3by1/2;
 //   // Calculate eW (angular velocity error in body-fixed frame)
 //   Vector3d eW = W - R.transpose() * Rd * Wd;
-//   node.eW = eW;
+//   _params.eW = eW;
 //   // Update integral term of control
 //   // Attitude:
-//   node.eiR = node.eiR_last + node.del_t * node.eR;
-//   node.eiR_last = node.eiR;
+//   _params.eiR = _params.eiR_last + _params.del_t * _params.eR;
+//   _params.eiR_last = _params.eiR;
 //   // Calculate 3 DOFs of M (controlled moment in body-fixed frame)
 //   // MATLAB: M = -kR*eR-kW*eW-kRi*eiR+cross(W,J*W)+J*(R'*Rd*Wddot-hat(W)*R'*Rd*Wd);
 //   Matrix3d What;
 //   eigen_skew(W, What);
 //
-//   Vector3d M = -node.kR * node.eR - node.kW * eW - node.kiR * node.eiR + W.cross(node.J*W) + node.J*(R.transpose()*Rd*Wddot - What*R.transpose()*Rd*Wd) - M_g;
+//   Vector3d M = -_params.kR * _params.eR - _params.kW * eW - _params.kiR * _params.eiR + W.cross(_params.J*W) + _params.J*(R.transpose()*Rd*Wddot - What*R.transpose()*Rd*Wd) - M_g;
 //   // To try different motor speeds, choose a force in the radial direction
-//   double f = node.m*node.g;// N
-//   node.f_total = f;
+//   double f = _params.m*_params.g;// N
+//   _params.f_total = f;
 //   // Convert forces & moments to f_i for i = 1:6 (forces of i-th prop)
 //   VectorXd FM(4);
 //   FM << f, M(0), M(1), M(2);
-//   node.M = M;
-//   node.f_motor = node.Ainv * FM;
+//   _params.M = M;
+//   _params.f_motor = _params.Ainv * FM;
 // }
